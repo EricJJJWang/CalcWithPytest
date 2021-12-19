75c59ff140c5f6f61ca89511586ac51c96d0e018
Eric W <wes830129@gmail.com>
2021-12-19 17:55:26 +0800

    add add() to test_01

--- test_01.py
+++ test_01.py
@@ -1,8 +1,10 @@
 import pytest
 
-
 def func(x):
     return x + 1
+
+def add(x, y):
+    return x + y
 
 
 def test_answer():
@@ -10,6 +12,9 @@ def test_answer():
 
 def test_answer1():
     assert func(3) == 4
+
+def test_add():
+    assert add(1,1) ==2
 
 if __name__ == '__main__':
     pytest.main(['-s','test_01.py'])
